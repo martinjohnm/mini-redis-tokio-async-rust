290c40c7df6fe896ee1fea068f5464d68c934b65
Martin John M <johnmmartin74@gmail.com>
2026-03-05 21:37:49 +0100

    Added the main function using tokio and a process fn to accept the incoming connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use tokio::net::{TcpListener, TcpStream};
+
 
 
 
@@ -6,5 +8,18 @@
 #[tokio::main]
 async fn main() {
     
+    // bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, ip) = listener.accept().await.unwrap();
+        println!("{:?}", ip);
+        process(socket).await;
+
+    }
+    
+}
+
+async fn process(socket : TcpStream) {
     
 }
