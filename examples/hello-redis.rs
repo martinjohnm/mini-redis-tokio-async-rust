5f5fa8ccf5971265833348c779e65c6d2c3d5d80
Martin John M <johnmmartin74@gmail.com>
2026-03-05 21:32:44 +0100

    :

---
+++ examples/hello-redis.rs
@@ -0,0 +1,24 @@
+
+
+
+
+use mini_redis::{client, Result};
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    // open a connection to mini-redis address
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // set the new key "hello" with value world
+    client.set("hello", "world".into()).await?;
+
+    // get the key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result = {:?}", result);
+
+    Ok(())
+
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+
+
+
+
+
+#[tokio::main]
+async fn main() {
+    
+    
+}
